87da384c405f677efc7d7b3ea5025fb4585304ae
thanusuyasris <84395546+thanusuyasris@users.noreply.github.com>
2021-06-06 23:50:18 +0530

    Add files via upload

---
+++ src/components/Note.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1> Be happy for this moment. 
+        This moment is your life.</h1>
+      <p>
+        {" "}
+        Happiness is an emotional state characterized by feelings of joy,
+        satisfaction, contentment, and fulfillment. While happiness has many
+        different definitions, it is often described as involving positive
+        emotions and life satisfaction.{" "}
+      </p>
+    </div>
+  );
+}
+
+export default Note;
